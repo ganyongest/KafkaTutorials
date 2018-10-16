0e043d0b9467f07f8b279e221ae02254b0417c14
yqbjtu <yqbjtu@163.com>
2018-10-16 17:13:01 +0800

    KafkaListener for concurrentListener

--- concurrentListener/src/main/java/com/yq/consumer/MyListener.java
+++ concurrentListener/src/main/java/com/yq/consumer/MyListener.java
@@ -20,10 +20,12 @@ import java.util.concurrent.CountDownLatch;
 @Slf4j
 public class MyListener {
     private static final String TPOIC = "topic02";
+    //CountDownLatch纯粹是为了测试方便，实际中完全可以不用
     public CountDownLatch countDownLatch0 = new CountDownLatch(3);
     public CountDownLatch countDownLatch1 = new CountDownLatch(3);
     public CountDownLatch countDownLatch2 = new CountDownLatch(3);
 
+    //@KafkaListener(topicPattern = "iiot.1prod.*", groupId = "group1")  这种方式也行
     @KafkaListener(id = "id0", topicPartitions = { @TopicPartition(topic = TPOIC, partitions = { "0" }) })
     public void listenPartition0(ConsumerRecord<?, ?> record) {
         System.out.println("Listener Id0, Thread ID: " + Thread.currentThread().getId());
